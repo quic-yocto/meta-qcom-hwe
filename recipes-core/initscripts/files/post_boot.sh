ccd0d873ec1e7206ea1ac462276d92fc5821d393
Jagadeesh Pagadala <quic_jpagadal@quicinc.com>
2024-08-26 16:42:37 +0530

    post-boot: update scaling governor for all policy nodes
    Update the scaling governor for all the clusters available.
    Also, correcting the post-boot service description.
    
    Change-Id: Ib46ca31ff9e96e360bd7694f78e2a354b4bb9819
    Signed-off-by: Jagadeesh Pagadala <quic_jpagadal@quicinc.com>

--- recipes-core/initscripts/files/post_boot.sh
+++ recipes-core/initscripts/files/post_boot.sh
@@ -2,9 +2,9 @@
 # Copyright (c) 2023-2024, Qualcomm Innovation Center, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 
-echo schedutil > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
-echo schedutil > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor
-echo schedutil > /sys/devices/system/cpu/cpufreq/policy7/scaling_governor
+for policy in `ls -d /sys/devices/system/cpu/cpufreq/policy*`;
+    do echo schedutil > $policy/scaling_governor;
+done
 
 echo 128 > /proc/sys/kernel/sched_util_clamp_min_rt_default
 
