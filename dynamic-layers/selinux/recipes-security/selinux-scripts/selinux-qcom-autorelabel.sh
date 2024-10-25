ce4158f85b08d6b8bc2abd3fd9fd2830bae5b464
Ravi Kumar Siddojigari <quic_rsiddoji@quicinc.com>
2024-10-25 16:48:48 +0530

    sepolicy : update fc entries for ostree related paths .
    ostree keep  hash based folder with .origin and .x sort of
    folder so adding them to fc so that they get labeled correctly .
    Adding some misc changes that are needed with cleanup.
    
    Change-Id: Ie81fdd130b2ea001a90cc17d79de7eb1c4de7c78
    Signed-off-by: Ravi Kumar Siddojigari <quic_rsiddoji@quicinc.com>

--- dynamic-layers/selinux/recipes-security/selinux-scripts/selinux-qcom-autorelabel.sh
+++ dynamic-layers/selinux/recipes-security/selinux-scripts/selinux-qcom-autorelabel.sh
@@ -1,45 +1,19 @@
 #!/bin/sh
 
-# Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause-Clear
-
 /usr/sbin/selinuxenabled 2>/dev/null || exit 0
 
-SETFILES=/usr/sbin/setfiles
-SETENFORCE=/usr/sbin/setenforce
-FIXFILES=/usr/sbin/fixfiles
 RESTORCON=/usr/sbin/restorecon
 
-for i in ${FIXFILES} ${SETENFORCE}; do
-	test -x $i && continue
-	echo "$i is missing in the system."
-	echo "Please add \"selinux=0\" in the kernel command line to disable SELinux."
-	exit 1
-done
+#Will run only for the first time and once done ostree will create this file
+# /.updated  different from upstream autorelabel and this will not trigger reboot
 
-# If /.autorelabel placed, the whole file system should be relabeled
+if [ ! -f /etc/.updated ]; then
+       ${RESTORCON} -R  /etc/tmpfiles.d/00ostree-tmpfiles.conf 
+       ${RESTORCON} -R  /var  /etc
+       ${RESTORCON} -R  /media /mnt   /root  /home
+       ${RESTORCON} -R  /usr/local
+       ${RESTORCON} -R  /var/rootdirs
+       ${RESTORCON}   /sysroot
 
-if [ -f /.autorelabel ]; then
-        echo "SELinux: /.autorelabel placed, filesystem will be relabeled..."
-        start_time=$(date +%s)
-        ${SETENFORCE} 0
-        source /usr/etc/selinux/config  > /dev/null
-#       ${SETFILES}  -v -i -F -r  /usr /usr/etc/selinux/${SELINUXTYPE}/contexts/files/file_contexts  /usr
-        sed -i 's|/bin/sh|/bin/bash|1' /usr/sbin/fixfiles
-#       ${FIXFILES} -F -f relabel
-####  OSTree expected  not to touch  /sysroot  /   so
-      ${RESTORCON}  -v -R   /var  /etc
-      ${RESTORCON}  -v -R /usr/local
-      ${RESTORCON}  -v -R /var/rootdirs/media
-      ${RESTORCON}  -v -R /var/rootdirs/mnt
-      ${RESTORCON}  -v -R /var/rootdirs/media
-      ${RESTORCON}  -v -R /var/rootdirs/opt
-
-        /bin/rm -f /.autorelabel
-        end_time=$(date +%s)
-        execution_time=$(expr $end_time - $start_time)
-        echo " * Time taken :  $execution_time Relabel done, rebooting the system."
-        /sbin/reboot
 fi
-
 exit 0
