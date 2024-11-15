6ee75dc15b01b7fd6d9003401bfcd08cb5e6a1e5
Roshan Sivakumar <quic_roshs@quicinc.com>
2024-11-15 13:34:28 +0530

    Create /var/rootdirs/home/root with selinux-qcom-autorelabel.sh
    Create /var/rootdirs/home/root directory as part of
    selinux-qcom-autorelabel.sh and set the permissions of the
    directory to 0700.
    
    Change-Id: If75e5c3f36e1c764e5df1efa90dd48de04f92051
    Signed-off-by: Roshan Sivakumar <quic_roshs@quicinc.com>

--- dynamic-layers/selinux/recipes-security/selinux-scripts/selinux-qcom-autorelabel.sh
+++ dynamic-layers/selinux/recipes-security/selinux-scripts/selinux-qcom-autorelabel.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
-
 /usr/sbin/selinuxenabled 2>/dev/null || exit 0
-
 RESTORCON=/usr/sbin/restorecon
+
+mkdir -p /var/rootdirs/home/root
+${RESTORCON} -R  /var/rootdirs
 
 #Will run only for the first time and once done ostree will create this file
 # /.updated  different from upstream autorelabel and this will not trigger reboot
-
 if [ ! -f /etc/.updated ]; then
-       ${RESTORCON} -R  /etc/tmpfiles.d/00ostree-tmpfiles.conf 
+       ${RESTORCON} -R  /etc/tmpfiles.d/00ostree-tmpfiles.conf
        ${RESTORCON} -R  /var  /etc
        ${RESTORCON} -R  /media /mnt   /root  /home
        ${RESTORCON} -R  /usr/local
        ${RESTORCON} -R  /var/rootdirs
        ${RESTORCON}   /sysroot
-
 fi
 exit 0
