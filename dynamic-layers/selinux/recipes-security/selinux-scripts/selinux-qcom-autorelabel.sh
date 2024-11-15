7cb1a82ed88946b27ca4f0dd6d1ffe2a5ecf4a1e
Roshan Sivakumar <quic_roshs@quicinc.com>
2024-11-15 13:46:10 +0530

    Label files on the first boot of each OSTree deployment
    This script creates a cookie specific to each deployment
    based on it's hash like restore-<hash> so that it labels
    for every new deployment, but only for the first boot.
    
    Change-Id: Iecab8ac83bb8e56be27de3b891b38d3c1b890e3d
    Signed-off-by: Roshan Sivakumar <quic_roshs@quicinc.com>

--- dynamic-layers/selinux/recipes-security/selinux-scripts/selinux-qcom-autorelabel.sh
+++ dynamic-layers/selinux/recipes-security/selinux-scripts/selinux-qcom-autorelabel.sh
@@ -5,14 +5,21 @@ RESTORCON=/usr/sbin/restorecon
 mkdir -p /var/rootdirs/home/root
 ${RESTORCON} -R  /var/rootdirs
 
-#Will run only for the first time and once done ostree will create this file
-# /.updated  different from upstream autorelabel and this will not trigger reboot
-if [ ! -f /etc/.updated ]; then
+current_deployment=$(ostree admin status | awk '/^\*/ {print $3}')
+if [ -n "$current_deployment" ]; then
+       restore_cookie="/etc/restore-$current_deployment"
+fi
+
+#Will run only for the first time and once done it will create a cookie
+#of the form restore-hash specific to each deployment
+if [ ! -f "$restore_cookie" ]; then
        ${RESTORCON} -R  /etc/tmpfiles.d/00ostree-tmpfiles.conf
        ${RESTORCON} -R  /var  /etc
        ${RESTORCON} -R  /media /mnt   /root  /home
        ${RESTORCON} -R  /usr/local
        ${RESTORCON} -R  /var/rootdirs
        ${RESTORCON}   /sysroot
+       touch "$restore_cookie"
+       /sbin/reboot
 fi
 exit 0
