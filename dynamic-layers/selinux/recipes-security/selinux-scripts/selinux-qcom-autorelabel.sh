2e6cac358cf5ae3985bf5d6141ebfae6b97da80f
Roshan Sivakumar <quic_roshs@quicinc.com>
2024-11-16 18:58:02 +0530

    Label only for first boot and trigger reboot
    Change-Id: I6b66f202185478e8f4bd9e3fc558a52358ddff2b
    Signed-off-by: Roshan Sivakumar <quic_roshs@quicinc.com>

--- dynamic-layers/selinux/recipes-security/selinux-scripts/selinux-qcom-autorelabel.sh
+++ dynamic-layers/selinux/recipes-security/selinux-scripts/selinux-qcom-autorelabel.sh
@@ -5,21 +5,16 @@ RESTORCON=/usr/sbin/restorecon
 mkdir -p /var/rootdirs/home/root
 ${RESTORCON} -R  /var/rootdirs
 
-current_deployment=$(ostree admin status | awk '/^\*/ {print $3}')
-if [ -n "$current_deployment" ]; then
-       restore_cookie="/etc/restore-$current_deployment"
-fi
-
-#Will run only for the first time and once done it will create a cookie
-#of the form restore-hash specific to each deployment
-if [ ! -f "$restore_cookie" ]; then
-       ${RESTORCON} -R  /etc/tmpfiles.d/00ostree-tmpfiles.conf
+#Will run only for the first time and once done ostree will create this file
+#/etc/labeldone
+if [ ! -f /etc/labeldone ]; then
+       ${RESTORCON} -R  /etc/tmpfiles.d/00ostree-tmpfiles.conf 
        ${RESTORCON} -R  /var  /etc
        ${RESTORCON} -R  /media /mnt   /root  /home
        ${RESTORCON} -R  /usr/local
        ${RESTORCON} -R  /var/rootdirs
        ${RESTORCON}   /sysroot
-       touch "$restore_cookie"
+       touch /etc/labeldone
        /sbin/reboot
 fi
 exit 0
