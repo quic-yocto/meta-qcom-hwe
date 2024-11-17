2b6cf776b547d8c4c681a3c5336a96c6499702b2
Roshan Sivakumar <quic_roshs@quicinc.com>
2024-11-17 20:29:49 +0530

    selinux-qcom-autorelabel.sh: Create cookie specific to each deployment
    Change-Id: Id80e94a3e02e41952fa916f5f0cf6b6719ba2002
    Signed-off-by: Roshan Sivakumar <quic_roshs@quicinc.com>

--- dynamic-layers/selinux/recipes-security/selinux-scripts/selinux-qcom-autorelabel.sh
+++ dynamic-layers/selinux/recipes-security/selinux-scripts/selinux-qcom-autorelabel.sh
@@ -2,19 +2,26 @@
 /usr/sbin/selinuxenabled 2>/dev/null || exit 0
 RESTORCON=/usr/sbin/restorecon
 
+sleep 5
 mkdir -p /var/rootdirs/home/root
 ${RESTORCON} -R  /var/rootdirs
 
+current_deployment=$(ostree admin status | awk '/^\*/ {print $3}')
+if [ -n "$current_deployment" ]; then
+       restore_cookie="/etc/restore-$current_deployment"
+fi
+
 #Will run only for the first time and once done ostree will create this file
 #/etc/labeldone
-if [ ! -f /etc/labeldone ]; then
+if [ ! -f "$restore_cookie" ]; then
        ${RESTORCON} -R  /etc/tmpfiles.d/00ostree-tmpfiles.conf 
        ${RESTORCON} -R  /var  /etc
        ${RESTORCON} -R  /media /mnt   /root  /home
        ${RESTORCON} -R  /usr/local
        ${RESTORCON} -R  /var/rootdirs
        ${RESTORCON}   /sysroot
-       touch /etc/labeldone
+       touch "$restore_cookie"
        /sbin/reboot
 fi
+
 exit 0
